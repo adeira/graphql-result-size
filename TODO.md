1292b7ae0351d40e4573156fbd115221f48f0226
Martin Zlámal <mrtnzlml@gmail.com>
2019-10-30 21:02:31 -0700

    Flatten the directory structure
    adeira-source-id: 6c4248f2507265454a8c21949c6cef09c911cd32

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- implement it in the Relay Compiler (first as a validator?)
+- apply optimizations described in the paper (do not visit already known node twice)
+- ...
---
+++ src/__tests__/resultSizeRealQuery.test.js
@@ -0,0 +1,17 @@
+// @flow
+
+import fs from 'fs';
+import path from 'path';
+import { parse, buildSchema } from 'graphql';
+import { generateTestsFromFixtures } from '@kiwicom/test-utils';
+
+import { calculate } from '../calculate';
+
+const schema = buildSchema(
+  fs.readFileSync(path.join(__dirname, 'testschema-real.graphql'), 'utf8'),
+);
+
+generateTestsFromFixtures(path.join(__dirname, '__fixtures__'), input => {
+  const score = calculate(schema, parse(input));
+  return `✅ Score: ${score}`;
+});
